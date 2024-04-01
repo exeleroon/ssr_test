76e0f79b964e92a35ab9376e687752d1b0ab53b3
Igor Bul <exeleroon@gmail.com>
2024-04-01 20:35:13 +0300

    dd

--- src/server/index.js
+++ src/server/index.js
@@ -57,80 +57,64 @@ const readCssFiles = () => {
 };
 
 app.get('*', async (req, res) => {
-    const appString = renderToString(React.createElement(App));
+    try {
+        const context = {};
+        const splitRoute = req.originalUrl.split('/');
+        const getTitleDesc = routesArr.find(route => route.path.includes(splitRoute[1]));
 
-    // Read the compiled index.html file
-    const indexPath = path.resolve(__dirname, '../dist/index.html');
-    let indexHtml = fs.readFileSync(indexPath, 'utf8');
+        const getUserId = req.originalUrl.split('/').slice(-1)[0];
+        let userId;
+        if (!isNaN(parseInt(getUserId))) {
+            userId = getUserId;
+        }
 
-    // Inject the client-side bundle into the HTML
-    const clientBundlePath = '/bundle.js'; // Path to your compiled client-side bundle
-    indexHtml = indexHtml.replace('<head>', `<head><script src="${clientBundlePath}"></script>`);
+        const [customCss] = await readCssFiles();
+        const response = await axios.get('https://jsonplaceholder.typicode.com/users');
+        const users = response.data;
 
-    // Replace the placeholder with the rendered React app
-    indexHtml = indexHtml.replace('<div id="root"></div>', `<div id="root">${appString}</div>`);
-    res.send(indexHtml);
+        const responsePosts = await axios.get('https://jsonplaceholder.typicode.com/posts?userId=' + userId);
+        const posts = responsePosts.data;
 
+        const responseAlbums = await axios.get('https://jsonplaceholder.typicode.com/albums?userId=' + userId);
+        const albums = responseAlbums.data;
 
-    //
-    // try {
-    //     const context = {};
-    //     const splitRoute = req.originalUrl.split('/');
-    //     const getTitleDesc = routesArr.find(route => route.path.includes(splitRoute[1]));
-    //
-    //     const getUserId = req.originalUrl.split('/').slice(-1)[0];
-    //     let userId;
-    //     if (!isNaN(parseInt(getUserId))) {
-    //         userId = getUserId;
-    //     }
-    //
-    //     const [customCss] = await readCssFiles();
-    //     const response = await axios.get('https://jsonplaceholder.typicode.com/users');
-    //     const users = response.data;
-    //
-    //     const responsePosts = await axios.get('https://jsonplaceholder.typicode.com/posts?userId=' + userId);
-    //     const posts = responsePosts.data;
-    //
-    //     const responseAlbums = await axios.get('https://jsonplaceholder.typicode.com/albums?userId=' + userId);
-    //     const albums = responseAlbums.data;
-    //
-    //     if (splitRoute[1] !== '?' && getTitleDesc && userId) {
-    //         const getUser = users.find(user => parseInt(user.id) === parseInt(userId));
-    //         getTitleDesc.description = getTitleDesc.description + getUser?.name;
-    //         getTitleDesc.title = getUser?.username + ' ' + getTitleDesc.title;
-    //     }
-    //
-    //     users.sort(function (a, b) {
-    //         if (a.name < b.name) {
-    //             return -1;
-    //         }
-    //         if (a.name > b.name) {
-    //             return 1;
-    //         }
-    //         return 0;
-    //     })
-    //
-    //     const appString = renderToString(
-    //         <Provider store={store}>
-    //             <StaticRouter location={req.url} context={context}>
-    //                 <App users={users} albums={albums} posts={posts}/>
-    //             </StaticRouter>
-    //         </Provider>
-    //     );
-    //
-    //     const html = template(appString, getTitleDesc, customCss);
-    //     res.send(html);
-    //
-    // } catch (error) {
-    //     console.error('Error fetching users:', error);
-    //     res.status(500).send('Internal Server Error');
-    // }
+        if (splitRoute[1] !== '?' && getTitleDesc && userId) {
+            const getUser = users.find(user => parseInt(user.id) === parseInt(userId));
+            getTitleDesc.description = getTitleDesc.description + getUser?.name;
+            getTitleDesc.title = getUser?.username + ' ' + getTitleDesc.title;
+        }
+
+        users.sort(function (a, b) {
+            if (a.name < b.name) {
+                return -1;
+            }
+            if (a.name > b.name) {
+                return 1;
+            }
+            return 0;
+        })
+
+        const appString = renderToString(
+            <Provider store={store}>
+                <StaticRouter location={req.url} context={context}>
+                    <App users={users} albums={albums} posts={posts}/>
+                </StaticRouter>
+            </Provider>
+        );
+
+        const html = template(appString, getTitleDesc, customCss);
+        res.send(html);
+
+    } catch (error) {
+        console.error('Error fetching users:', error);
+        res.status(500).send('Internal Server Error');
+    }
 });
 
 app.get('/posts/:userId');
 app.get('/albums/:userId');
 
 
-app.listen('http://localhost:3000', () => {
+app.listen('http://localhost:3000/', () => {
     console.log('Server is listening on PORT 3000');
 });
